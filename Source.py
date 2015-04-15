27db5df8d2eebccdc7937a1174c035ad98e022be
benjaminh <benjamin.hervy@irccyn.ec-nantes.fr>
2015-04-15 18:03:47 +0200

    Mise en forme des fichiers conformément à PEP8

--- Source.py
+++ Source.py
@@ -3,25 +3,28 @@
 #
 from py2neo import Graph, GraphError, Node, Relationship
 
+
 class Source(object):
 
-	def __init__(self, node):
-		self._node = node
+    def __init__(self, node):
+        self._node = node
 
-	@classmethod
-	def create_source(self,graph_db,type_source,ref_source,filename,auteur,contenu):
+    @classmethod
+    def create_source(self, graph_db, type_source,
+                      ref_source, filename, auteur, contenu):
 
-		self.node_type = "Source"
-	
-		# Ajouter propriétés du type "modified" ?
-		source_properties = {'ref_source':ref_source,'auteur':auteur,'legende':contenu}
-		source_node = Node.cast(fiche_properties)
-		source_node.labels.add(self.node_type)
-		source_node.labels.add(type_source)
-		graph_db.create(source_node)
+        self.node_type = "Source"
 
-		return Source(source_node)
+        # Ajouter propriétés du type "modified" ?
+        source_properties = {'ref_source': ref_source, 'auteur':
+                             auteur, 'legende': contenu}
+        source_node = Node.cast(fiche_properties)
+        source_node.labels.add(self.node_type)
+        source_node.labels.add(type_source)
+        graph_db.create(source_node)
 
-	@property 
-	def legende(self): 
-		return self._node["legende"]
+        return Source(source_node)
+
+    @property
+    def legende(self):
+        return self._node["legende"]
