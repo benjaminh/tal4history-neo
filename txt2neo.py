4f344ca3e3441ffc88a598bbb98f3a97811434f0
benjaminh <benjamin.hervy@irccyn.ec-nantes.fr>
2015-04-15 15:43:09 +0200

    [TEST] Ajout d'un fichier vide pour la connexion à Neo4j

---
+++ txt2neo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+from py2neo import Graph, Node, Relationship
+
+def nodes(neo_db):
+	#À ÉCRIRE
+
+def edges(neo_db):
+	# À ÉCRIRE	
+
+def main():
+	graph_db = Graph()
+
+if __name__ == "__main__":
+	main()
